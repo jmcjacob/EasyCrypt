538233ce812cd5f64f52be3a13b4843bcbe17df7
thinh <thing@thing.com>
2015-03-19 15:07:46 +0000

    Comments

--- WindowsFormsApplication1/Form3.cs
+++ WindowsFormsApplication1/Form3.cs
@@ -12,6 +12,7 @@ namespace WindowsFormsApplication1
 {
     public partial class Form3 : Form
     {
+        // This is a comment 
         public Form3()
         {
             InitializeComponent();
